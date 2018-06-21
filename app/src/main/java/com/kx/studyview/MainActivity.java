9d41b29fdb8e49075059f3a921802a0c61d3bc3b
kk9923 <317168664@qq.com>
2018-06-21 22:43:40 +0800

    创建水波纹界面，准备学习水波纹效果，通过(正余弦函数 + 贝塞尔曲线)来实现。

--- app/src/main/java/com/kx/studyview/MainActivity.java
+++ app/src/main/java/com/kx/studyview/MainActivity.java
@@ -1,13 +1,12 @@
 package com.kx.studyview;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.kx.studyview.activity.ColorfulTextViewActivity;
+import com.kx.studyview.activity.WaveViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView colorfulTextView = findViewById(R.id.colorfulTextView);
-        colorfulTextView.setOnClickListener(mClickListener);
+        findViewById(R.id.colorfulTextView).setOnClickListener(mClickListener);
+        findViewById(R.id.waveView).setOnClickListener(mClickListener);
     }
     private View.OnClickListener mClickListener = new View.OnClickListener() {
         @Override
@@ -24,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId()){
                 case R.id.colorfulTextView:
                     startActivity(ColorfulTextViewActivity.class);
+                    break;
+                case R.id.waveView:
+                    startActivity(WaveViewActivity.class);
                     break;
             }
         }
---
+++ app/src/main/java/com/kx/studyview/activity/WaveViewActivity.java
@@ -0,0 +1,20 @@
+package com.kx.studyview.activity;
+
+import com.kx.studyview.BaseActivity;
+import com.kx.studyview.R;
+
+/**
+ * Created by admin on 2018/6/21.
+ */
+
+public class WaveViewActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public int getRootLayoutIds() {
+        return R.layout.activity_wave_view;
+    }
+}
